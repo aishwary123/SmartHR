d590e8761fb84b298cc75f2f351f1900c048e61f
aishwary123 <aishwary.muz@gmail.com>
2016-12-09 14:41:59 +0530

    Initial Commit for Project Setup

---
+++ src/main/java/com/sap/aish/SmartHrApplication.java
@@ -0,0 +1,12 @@
+package com.sap.aish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHrApplication.class, args);
+	}
+}
